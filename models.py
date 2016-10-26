4646cea206c9b89737526dfafd80797dc71e99e0
jakobsalej <jakob.salej@gmail.com>
2016-10-26 18:16:36 +0200

    Moved DB models to a separate file; fixed relative links in items' descriptions; added model Town.

---
+++ models.py
@@ -0,0 +1,71 @@
+from peewee import *
+import datetime
+
+
+
+def connectDB():
+    db = PostgresqlDatabase(
+        'slovenia_db',
+        user='adminslo',
+        password='slo',
+        host='localhost',
+    )
+
+    return db
+
+
+class BaseModel(Model):
+    class Meta:
+        database = connectDB()
+
+
+class Region(BaseModel):
+    name = CharField()
+    link = CharField()
+    description = TextField()
+    picture = CharField()
+    timestamp = DateTimeField(default=datetime.datetime.now)
+
+
+class Attraction(BaseModel):
+    name = CharField()
+    link = CharField()
+    address = CharField()
+    phone = CharField()
+    webpage = CharField()
+    tags = CharField()
+    type = CharField()
+    description = TextField()
+    picture = CharField()
+    regionName = CharField()
+    region = ForeignKeyField(Region, related_name='attractions')
+    destination = CharField()
+    place = CharField()
+    gpsX = DoubleField()
+    gpsY = DoubleField()
+    timestamp = DateTimeField(default=datetime.datetime.now)
+
+
+class Town(BaseModel):
+    name = CharField()
+    link = CharField()
+    webpage = CharField()
+    population = IntegerField()
+    altitude = CharField()
+    position = CharField()
+    tempSummer = CharField()
+    tempWinter = CharField()
+    sunnyDays = IntegerField()
+    rainyDays = IntegerField()
+    tags = CharField()
+    type = CharField()
+    description = TextField()
+    picture = CharField()
+    regionName = CharField()
+    region = ForeignKeyField(Region, related_name='towns')
+    destination = CharField()
+    place = CharField()
+    gpsX = DoubleField()
+    gpsY = DoubleField()
+    topResult = BooleanField()
+    timestamp = DateTimeField(default=datetime.datetime.now)

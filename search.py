bd00335234b4d2a9da0fab90e6e0dc9b40939ba5
Jakob Salej <jakob.salej@gmail.com>
2017-03-22 19:27:54 +0100

    Fixed a bug when searching for regions.

--- search.py
+++ search.py
@@ -110,6 +110,7 @@ def init():
             id=str(region.id).encode("utf-8").decode("utf-8"),
             name=region.name,
             link=region.link,
+            webpage='',
             description=region.description,
             picture=region.picture,
             regionName=region.name,    # just for displaying results, doesn't matter
@@ -836,14 +837,14 @@ def setDefaultLocationFilter(location, field):
 #init()
 
 # testing search
-index = open_dir("index")
+#index = open_dir("index")
 
 # get slovenian towns from file
-with open('kraji_slovenija', 'rb') as fp:
+with open('../kraji_slovenija', 'rb') as fp:
     townsStatic = pickle.load(fp)
 
 
-results = analyzeQuery(index, 'seznam rek ob morju')
+#results = analyzeQuery(index, 'gorenjska')
 #results = analyzeQuery(index, 'znamenitosti v blizini')
 #results = analyzeQuery(index, 'seznam arhitekture')
 #results = analyzeQuery(index, 'grat')
@@ -852,5 +853,3 @@ results = analyzeQuery(index, 'seznam rek ob morju')
 #results = analyzeQuery(index, 'povej mi kaj o bledu')
 #results = analyzeQuery(index, 'arhitektura ljubljana')
 #results = analyzeQuery(index, 'ljubljna')   #!!!
-
-print(calculatePrefix('morju'))
